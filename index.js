12a459b7639450fbcc5fba6919fd8232a745615e
ratan <ratanchakma68@gmail.com>
2024-02-16 22:50:33 +0600

    li element add

--- index.js
+++ index.js
@@ -16,8 +16,30 @@ const grandTotal = getElementID("grand-total");
 // card button finding
 const cards = document.querySelectorAll(".add-btn");
 for (const card of cards) {
-    card.addEventListener("click", () => {
-       chartCount.innerText++;
+    card.addEventListener("click", (event) => {
+        chartCount.innerText++;
+// find place and price value
+        const place = event.target.parentNode.childNodes[1].innerText;
+        const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
+// create li element
+        const li = document.createElement("li");
+         li.innerHTML =`${place} ${parseInt(price)}`;
+        const test = selectedPriceContainer.appendChild(li);
+        test.classList.add("li-style");
+        
+
+
+
 
     })
+}
+
+
+
+
+
+// create list function
+const createList = () => {
+    const li = document.createElement("li");
+    li.createTextNode = ``
 }
